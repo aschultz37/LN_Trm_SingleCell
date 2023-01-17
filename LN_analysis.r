c67f3afb893d5357568f8bec9e67624c1e518a4a
aschultz37 <austin.schultz@nyulangone.org>
2023-01-17 11:51:44 -0500

    code to remove and reanalyze clusters

--- LN_analysis.r
+++ LN_analysis.r
@@ -169,11 +169,13 @@ genelist = c("Itgae", "Ccr7", "Klf2", "Cxcr6", "S1pr1",
              "Cd8a", "Thy1", "Ptprc", "Cd3e")
 VlnPlot(scobj, features=genelist)
 
-# plot raw counts
+# plot log transformed counts
 VlnPlot(scobj, features=genelist, slot="counts", log=TRUE)
 
 # visualize feature expression on tSNE or PCA plot
 FeaturePlot(scobj, features=genelist)
+# to detect naive T cells
+FeaturePlot(scobj, features="Cd44")
 
 # N.B. can also try RidgePlot, CellScatter, and DotPlot to view dataset
 # ridge plot
@@ -196,3 +198,72 @@ DoHeatmap(scobj, features=top10$gene) + NoLegend()
 
 # save again, includes plots
 saveRDS(scobj, file="LN/output/2022-12-29_LN_figures_rp.rds")
+
+# REMOVE CLUSTERS AND RE-ANALYZE
+# Remove clusters 4, 7, 8 and keep the remaining for new analysis
+sub1_scobj = subset(x=scobj, idents=c(4, 7, 8), invert=TRUE)
+
+# reidentify highly variable features
+sub1_scobj <- FindVariableFeatures(sub1_scobj, selection.method="vst", nfeatures=2500)
+
+# find top 10 most variable genes
+sub1_top10 <- head(VariableFeatures(sub1_scobj), 10)
+
+# rescale data
+# results of scaling are stores in sub1_scobj[["RNA"]]@scale.data
+sub1_all.genes <- rownames(sub1_scobj)
+sub1_scobj <- ScaleData(sub1_scobj, features=sub1_all.genes)
+
+# re-run pca analysis
+sub1_scobj <- RunPCA(sub1_scobj, features=VariableFeatures(object=sub1_scobj))
+
+# determines # of PC to include (low p val); removes noise
+sub1_scobj <- JackStraw(sub1_scobj, dims=30, num.replicate=100)
+sub1_scobj <- ScoreJackStraw(sub1_scobj, dims=1:30)
+
+# can visually determine dropoff in p-value after certain # PC
+JackStrawPlot(sub1_scobj, dims=1:30)
+
+# alternatively, can use elbow plot to determine which # PC capture most signal
+ElbowPlot(sub1_scobj, ndims=30)
+
+# recluster cells
+sub1_scobj <- FindNeighbors(sub1_scobj, dims=1:25)     # choose dim based on PCA
+sub1_scobj <- FindClusters(sub1_scobj, resolution=0.3) # resolution determines # clusters
+
+# run umap
+sub1_scobj <- RunUMAP(sub1_scobj, dims=1:25) #choose dim based on PCA/Neighbors
+# note that you can set 'label = TRUE' or use LabelClusters function to
+# label the individual clusters
+DimPlot(sub1_scobj, reduction="umap")
+# print number of cells per cluster
+table(Idents(sub1_scobj))
+
+# find all markers
+sub1_scobj.markers <- FindAllMarkers(sub1_scobj, only.pos=TRUE, min.pct=0.25,
+                                logfc.threshold=0.25)
+
+# expression probability distributions across clusters
+sub1_genelist = c("Itgae", "Ccr7", "Klf2", "Cxcr6", "S1pr1", 
+             "Cd8a", "Thy1", "Ptprc", "Cd3e")
+VlnPlot(sub1_scobj, features=sub1_genelist)
+
+# plot log transformed counts
+VlnPlot(sub1_scobj, features=sub1_genelist, slot="counts", log=TRUE)
+
+# visualize feature expression on tSNE or PCA plot
+FeaturePlot(sub1_scobj, features=sub1_genelist)
+# to detect naive T cells
+FeaturePlot(sub1_scobj, features="Cd44")
+
+# N.B. can also try RidgePlot, CellScatter, and DotPlot to view dataset
+# ridge plot
+RidgePlot(sub1_scobj, features=sub1_genelist, ncol=3)
+
+# generate expression heatmap for top 10 markers for each cluster
+sub1_scobj.markers %>%
+  group_by(cluster) %>%
+  top_n(n=10, wt=avg_log2FC) -> sub1_top10
+DoHeatmap(sub1_scobj, features=sub1_top10$gene) + NoLegend()
+
+saveRDS(sub1_scobj, "LN/output/2022-12-29_LN_clust_rm.rds")
