6f9b70e51e648fe128fe03dcca913b18dba98468
aschultz37 <austin.schultz@nyulangone.org>
2023-01-19 16:05:20 -0500

    add module score for LN_Trm

--- LN_analysis.r
+++ LN_analysis.r
@@ -1,6 +1,7 @@
 library(dplyr)
 library(Seurat)
 library(patchwork)
+library(RColorBrewer)
 
 # INITIALIZING SEURAT OBJECT
 # Set working directory
@@ -249,20 +250,20 @@ sub1_scobj.markers <- FindAllMarkers(sub1_scobj, only.pos=TRUE, min.pct=0.25,
 # expression probability distributions across clusters
 sub1_genelist = c("Itgae", "Ccr7", "Klf2", "Cxcr6", "S1pr1", 
              "Cd8a", "Thy1", "Ptprc", "Cd3e")
-LN_Trm_genes <- c("Acap1", "Actn2", "Amica1", "Arhgef1", "Atxn7l3b", "Aw112010", 
-                  "B4galnt1", "Bcl11b", "Cbx3", "Ccnd2", "Ccr10", "Cd27", "Cd7", 
-                  "Cd74", "Chd3", "Cirbp", "Clec2d", "Crot", "Csf1", "Cxcr3", 
-                  "Cxcr6", "Eif5", "Evl", "Fam189b", "Fubp1", "Fyb", "Gramd1a", 
-                  "Sema4a","Gstp1", "Shisa5", "H2-T23", "Sipa1", "Hmgn1", 
-                  "Slfn2", "Hmha1", "Sp100", "Hsp90b1", "Spcs2", "Id2", "Srrm2", 
-                  "Ifitm10", "Stap1", "Ikzf3", "Tbc1d10c", "Il16", "Tesc", 
-                  "Il18r1", "Tnfaip8", "Il7r", "Tnrc6a", "Irf2bpl", "Tsc22d4", 
-                  "Itgae", "Uba52", "Itgal", "Ucp2", "Itm2c", "Wbp1", "Lfng", 
-                  "Xist", "Lpar6", "Ypel3", "Lrrc58", "Zbtb7a", "Ltb", "Znrf1", 
-                  "Ly6a", "Ly6e", "Ly6g5b", "Malat1", "Mbnl1", "Mrpl52", "Mxd4", 
-                  "Mycbp2", "N4bp2l2", "N4bp2l2", "Ndufa3", "Ndufa5", "Nktr", 
-                  "Nudcd3", "Ogt", "Pdcd4", "Pdia3", "Pdia6", "Ptpn7", "Ptprc", 
-                  "Rapgef6", "Rbpj", "Rgs10", "Rpl15", "Rpl35", "Rpl38", 
+LN_Trm_genes <- c("Acap1", "Actn2", "Arhgef1", "Atxn7l3b",
+                  "B4galnt1", "Bcl11b", "Cbx3", "Ccnd2", "Ccr10", "Cd27", "Cd7",
+                  "Cd74", "Chd3", "Cirbp", "Clec2d", "Crot", "Csf1", "Cxcr3",
+                  "Cxcr6", "Eif5", "Evl", "Fam189b", "Fubp1", "Fyb", "Gramd1a",
+                  "Sema4a","Gstp1", "Shisa5", "H2-T23", "Sipa1", "Hmgn1",
+                  "Slfn2", "Sp100", "Hsp90b1", "Spcs2", "Id2", "Srrm2",
+                  "Ifitm10", "Stap1", "Ikzf3", "Tbc1d10c", "Il16", "Tesc",
+                  "Il18r1", "Tnfaip8", "Il7r", "Tnrc6a", "Irf2bpl", "Tsc22d4",
+                  "Itgae", "Uba52", "Itgal", "Ucp2", "Itm2c", "Wbp1", "Lfng",
+                  "Xist", "Lpar6", "Ypel3", "Lrrc58", "Zbtb7a", "Ltb", "Znrf1",
+                  "Ly6a", "Ly6e", "Ly6g5b", "Malat1", "Mbnl1", "Mrpl52", "Mxd4",
+                  "Mycbp2", "N4bp2l2", "N4bp2l2", "Ndufa3", "Ndufa5", "Nktr",
+                  "Nudcd3", "Ogt", "Pdcd4", "Pdia3", "Pdia6", "Ptpn7", "Ptprc",
+                  "Rapgef6", "Rbpj", "Rgs10", "Rpl15", "Rpl35", "Rpl38",
                   "Rps28", "Rps29", "Sash3")
 cytotoxic_gene_list = c("Prf1", "Gzmb", "Gzmk", "Ccl4", "Ccl5", "Csf1")
 traffic_gene_list = c("S1pr1", "Ccr7", "Cxcr4", "Cxcr3", "Cxcr6")
@@ -316,5 +317,10 @@ cluster2v01_sub1.markers <- FindMarkers(sub1_scobj, ident.1=2, ident.2=c(0, 1),
 cluster2v01_sub1.markers.sig <- subset(cluster2v01_sub1.markers, p_val_adj<=0.05)
 write.csv(cluster2v01_sub1.markers.sig, "LN/output/cluster2v01_sub1.csv", 
           row.names=TRUE)
+
+# calculate module score for feature expression
+sub1_scobj <- AddModuleScore(object=sub1_scobj, features=list(LN_Trm_genes),
+                             ctrl=100, name="LN_Trm")
+FeaturePlot(sub1_scobj, features="LN_Trm1",pt.size=2.5, cols=brewer.pal(n=11, name="RdBu"))
 
 saveRDS(sub1_scobj, "LN/output/2022-12-29_LN_sub1.rds")
