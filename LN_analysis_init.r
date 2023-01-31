8015dbc352be6994bd817266a5a1c539d8b4d966
aschultz37 <austin.schultz@nyulangone.org>
2023-01-31 10:51:39 -0500

    re-add removed genes to Trm signature

--- LN_analysis_init.r
+++ LN_analysis_init.r
@@ -21,7 +21,7 @@ LN_Trm_genes <- c("Acap1", "Actn2", "Arhgef1", "Atxn7l3b",
                   "Mycbp2", "N4bp2l2", "N4bp2l2", "Ndufa3", "Ndufa5", "Nktr",
                   "Nudcd3", "Ogt", "Pdcd4", "Pdia3", "Pdia6", "Ptpn7", "Ptprc",
                   "Rapgef6", "Rbpj", "Rgs10", "Rpl15", "Rpl35", "Rpl38",
-                  "Rps28", "Rps29", "Sash3")
+                  "Rps28", "Rps29", "Sash3", "Amica1", "Aw112010", "Hmha1")
 cytotoxic_gene_list = c("Prf1", "Gzmb", "Gzmk", "Ccl4", "Ccl5", "Csf1")
 traffic_gene_list = c("S1pr1", "Sell", "Ccr7", "Cxcr4", "Cxcr3", "Cxcr6")
 gen_gene_list = c("Itgae", "Ccr7", "Klf2", "Cxcr6", "S1pr1", 
